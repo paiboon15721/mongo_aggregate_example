537c305c72e3c25fef3bedcb5a7ba68a8159bc22
paiboon <paiboon15721@gmail.com>
2022-11-01 22:28:35 +0700

    Add example for pipe

--- src/index.js
+++ src/index.js
@@ -1,53 +1,98 @@
-const mongoose = require('mongoose')
+const _ = require('lodash/fp')
 const util = require('util')
-const { initMongoose } = require('./infra')
+const { data } = require('./data')
 
-const log = x => console.log(util.inspect(x, { colors: true, depth: null }))
+const log = x =>
+  console.log('>', util.inspect(x, { colors: true, depth: null }))
 
-const { Schema } = mongoose
+const results = _.pipe(
+  _.groupBy('oaName'),
+  _.tap(console.log),
+  _.entries,
+  _.map(([k, v]) => ({
+    oaName: k,
+    grandTotalCommission: _.sumBy('totalCommission')(v),
+    grandTotalPayment: _.sumBy('paymentAmount')(v),
+    billCycles: _.pipe(
+      _.filter(v => v.billCycle),
+      _.groupBy('billCycle'),
+      _.entries,
+      _.map(([k, v]) => ({
+        billCycle: k,
+        totalCommission: _.sumBy('totalCommission')(v),
+        totalPayment: _.sumBy('paymentAmount')(v),
+        details: v,
+      })),
+    )(v),
+  })),
+)(data)
 
-const InitialAssignSchema = new Schema(
-  { account_no: String, n: Number },
-  { strict: false },
-)
-const InitialAssign = mongoose.model(
-  'InitialAssign',
-  InitialAssignSchema,
-  'initial_assign',
-)
+log(results)
 
-const PaymentResultSchema = new Schema({ accountno: String }, { strict: false })
-const PaymentResult = mongoose.model(
-  'PaymentResult',
-  PaymentResultSchema,
-  'payment_result',
-)
-
-const perPage = 20
-
-const page = 3
-
-const app = async () => {
-  await initMongoose()
-  const initialAssigns = await InitialAssign.aggregate([
-    {
-      $lookup: {
-        from: 'payment_result',
-        localField: 'account_no',
-        foreignField: 'accountno',
-        as: 'accounts',
+const result = [
+  {
+    oaName: 'abc',
+    grandTotalCustomer: 10,
+    grandTotalCommission: 1000,
+    grandTotalPayment: 2000,
+    billCycles: [
+      {
+        billCycle: 14,
+        totalCustomer: 2,
+        totalCommission: 200,
+        totalPayment: 200,
+        details: [
+          {
+            subTotalCustomer: 1,
+            subTotalPayment: 100,
+            numberOfAccountSent: 2,
+            numberOfAccountProtect: 2,
+            percentageOfAccountProtect: 3,
+            details: [
+              {
+                accountStatus: 'Npl',
+              },
+            ],
+          },
+        ],
       },
-    },
-    { $sort: { n: 1 } },
-    { $skip: (page - 1) * perPage },
-    { $limit: perPage },
-  ])
+      {
+        billCycle: 20,
+        totalCustomer: 2,
+        totalCommission: 200,
+        totalPayment: 200,
+      },
+      {
+        billCycle: 30,
+        totalCustomer: 2,
+        totalCommission: 200,
+        totalPayment: 200,
+      },
+    ],
+  },
+]
 
-  const result = initialAssigns
-    .map(x => ({ ...x, accounts: x.accounts[0] }))
-    .map(x => x.n)
+// mappedData.forEach(console.log)
 
-  log({ result })
-}
+// const results = _.pipe(
+//   _.map(v => v * 10),
+//   _.filter(v => v <= 20),
+//   _.sum,
+//   _.multiply(10),
+// )([1, 1.5, 2, 3, 3])
 
-app()
+// console.log({ results })
+
+// const greet = fn => {
+//   fn('hello world')
+// }
+
+// const log = v => {
+//   console.log(v)
+// }
+
+// greet(v => {
+//   log(v)
+// })
+
+// greet(log)
